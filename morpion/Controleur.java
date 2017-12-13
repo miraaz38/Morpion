a7bbc162828e24d3d1766f0c65fdae3d422464e0
damien <damien@mbp-de-damien>
2017-12-13 13:50:58 +0100

    test

--- morpion/Controleur.java
+++ morpion/Controleur.java
@@ -5,10 +5,21 @@
  */
 package morpion;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  *
  * @author damien
  */
-public class Controleur {
+public class Controleur implements Observer{
+
+    @Override
+    public void update(Observable o, Object arg) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
 }
